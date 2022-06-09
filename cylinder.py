cdea1def749169f9287e384bad96472bdfaacfd9
Guddu <theguddu1730@gmail.com>
2022-06-09 18:31:51 +0530

    initial commit

---
+++ cylinder.py
@@ -0,0 +1,11 @@
+print("advanced calculator" )
+x="22/7*r*r*h"
+print("formula of volume of cylinder is  ",x)
+print("enter radius  (r)")
+n1=input()
+print("enter height (h)")
+n2=input()
+print("the volume of cylinder is  ")
+print(3.14*float(n1)*float(n1)*float(n2))
+
+
---
+++ hrs2sec.py
@@ -0,0 +1,7 @@
+print("ADVANCED GUDDU CALCULATOR")
+print("ENTER THE HOURS")
+n1=input()
+print("there is",  int(n1)*3600,"SEC in \n"  )
+print(n1, end=" HOURS\n")
+
+print("and", int(n1)*60, "MINUTES" )
